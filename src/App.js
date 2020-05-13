bb40fc432304f44bc4c6ad00f01fda925d8f47d0
Klaudia Budzyńska <klaudia.budzynska1@gmail.com>
2020-05-13 18:12:26 +0200

    Socket IO setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
 import logo from './logo.svg';
 import './App.css';
 
+const ENDPOINT = "http://127.0.0.1:4001";
+
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
