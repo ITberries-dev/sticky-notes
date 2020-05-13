da8b32cb730f9503b023fb6ad0619927d8fb113e
Klaudia Budzyńska <klaudia.budzynska1@gmail.com>
2020-05-13 18:20:51 +0200

    hotfix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './App.scss';
